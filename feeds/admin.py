f02eba0cea889e931aab9810726548f50770e462
Nevetha Prakash <nevethap@thoughtworks.com>
2016-08-17 15:26:39 +0530

    created app feeds with model rss feed

--- feeds/admin.py
+++ feeds/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import RSSFeed
 # Register your models here.
+
+admin.site.register(RSSFeed)
--- feeds/models.py
+++ feeds/models.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-
+class RSSFeed(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    published = models.DateTimeField()
+    link = models.URLField(max_length=200)
