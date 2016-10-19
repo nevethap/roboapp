946ef6e1890075d7ca33174490bf5e404989adcf
abhinav_kumar_thakur <athakur@thoughtworks.com>
2016-10-19 11:23:22 +0530

    abhinav updates effect of the news sentiment

--- robo_app/news_sentiment/aggregator.py
+++ robo_app/news_sentiment/aggregator.py
@@ -9,15 +9,21 @@ from robo_app.news_sentiment.sentiment import SentimentScorer
 class NewsAggregator:
     def get_live_feeds(self, keywords, newsGroup):
         feed = feedparser.parse('http://feeds.reuters.com/reuters/hotStocksNews')
+        count = 0;
+        sentiment_sum = 0
         for entry in feed.entries:
-                for keyword in keywords:
-                    if keyword in entry.title_detail.value:
-                        title = entry.title_detail.value
-                        article_link = entry.link
-                        publishedString = entry.published
-                        published = parser.parse(publishedString)
-                        article_text = NewsCrawler.crawl_article(NewsCrawler(), article_link)
-                        sentimentScore = SentimentScorer()
-                        sentiment_score = sentimentScore.scoreArticle(article_text)
-                        models.News(headline=title, timeStamp=published, url=article_link,
-                                    sentiment=sentiment_score['Polarity'], group=newsGroup).save()
+            for keyword in keywords:
+                if keyword in entry.title_detail.value:
+                    title = entry.title_detail.value
+                    article_link = entry.link
+                    publishedString = entry.published
+                    published = parser.parse(publishedString)
+                    article_text = NewsCrawler.crawl_article(NewsCrawler(), article_link)
+                    sentimentScore = SentimentScorer()
+                    sentiment_score = sentimentScore.scoreArticle(article_text)
+                    sentiment_sum += sentiment_score
+                    count += 1
+                    models.News(headline=title, timeStamp=published, url=article_link,
+                                sentiment=sentiment_score['Polarity'], group=newsGroup).save()
+        newsGroup.effect = sentiment_sum if count==0 else sentiment_sum/count
+        newsGroup.save()
